eaf183cd7d4202d2a2e62f0eb800fad7a833cdc0
Hao Chen <hche8093@uni.sydney.edu.au>
2022-11-10 23:58:59 +1100

    Add BaseModal component in vue-weather

--- vue-weather-app/src/App.vue
+++ vue-weather-app/src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import SiteNavigation from './components/SiteNavigation.vue';
 </script>
 
 <style lang="scss" scoped>
--- vue-weather-app/src/router/index.js
+++ vue-weather-app/src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
-const routes = [{}];
+const routes = [
+	{
+		path: "/",
+		name: "home",
+		component: HomeView,
+	},
+];
 
 const router = createRouter({
 	history: createWebHistory(process.env.BASE_URL),
