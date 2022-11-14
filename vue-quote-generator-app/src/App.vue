45e0bb1c062c75826e0d4cb97a6347a9c4d0c885
Hao Chen <hche8093@uni.sydney.edu.au>
2022-11-14 16:51:22 +1100

    Add components and css in vue-quoter-app

--- vue-quote-generator-app/src/App.vue
+++ vue-quote-generator-app/src/App.vue
@@ -1,12 +1,53 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div class="app">
+    <Header title="The Anime Quoter" />
   </div>
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: { Header },
+};
 </script>
 
-<style lang="sass">
+<style lang="scss">
+:root {
+  --primary: #d81e5b;
+  --secondary: #8a4fff;
+  --tertiary: #32cbff;
+  --dark: #131a26;
+  --light: #eee;
+  --grey: #848484;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
+}
+.button-container {
+  display: flex;
+  justify-content: center;
+  padding: 0px 32px;
+  margin: 64px auto;
+  button {
+    border: none;
+    outline: none;
+    background-color: var(--primary);
+    padding: 16px 32px;
+    border-radius: 99px;
+    color: var(--light);
+    font-size: 1.5em;
+    font-weight: 700;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition: 0.4s;
+    &:hover {
+      background-color: var(--secondary);
+    }
+  }
+}
 </style>
