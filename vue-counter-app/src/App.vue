3398042e153a463e323d0535e53a0c2d3cffe9e1
Hao Chen <hche8093@uni.sydney.edu.au>
2022-11-16 21:41:54 +1100

    Add vue-counter-app

--- vue-counter-app/src/App.vue
+++ vue-counter-app/src/App.vue
@@ -1,10 +1,31 @@
 <script setup>
+import { ref } from 'vue';
+
+const count = ref(0)
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <main>
+    <div>
+      <h1>Counter App</h1>
+      <h1>{{ count }}</h1>
+      <button @click="count++">+</button>
+      <button @click="count--">-</button>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+main {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
+div {
+  text-align: center;
+}
 </style>
