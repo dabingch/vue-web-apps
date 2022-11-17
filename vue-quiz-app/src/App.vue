d7f2252e5c395dbe89a0f97d647e95aa296cec90
Hao Chen <hche8093@uni.sydney.edu.au>
2022-11-17 18:29:57 +1100

    Add some css style in vue-quiz

--- vue-quiz-app/src/App.vue
+++ vue-quiz-app/src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div class="container">
+    <header>
+      <h1>Quizzes</h1>
+      <input type="text" placeholder="Search...">
+    </header>
+
+    <div class="options-container">
+      <div class="card">
+        <img src="" alt="">
+        <div class="card-text">
+          <h2>Math</h2>
+          <p>15 questions</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,5 +22,37 @@
 </script>
 
 <style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
 
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
 </style>
