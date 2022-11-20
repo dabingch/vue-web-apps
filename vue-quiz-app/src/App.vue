64a8c06561e3323bb36a69682d705ed9c7d57b10
Hao Chen <hche8093@uni.sydney.edu.au>
2022-11-20 21:52:32 +1100

    Update structure in vue-quiz-app

--- vue-quiz-app/src/App.vue
+++ vue-quiz-app/src/App.vue
@@ -18,41 +18,13 @@
 </template>
 
 <script setup>
-
+  import { RouterView } from "vue-router"
+  import QuizzesView from "./views/QuizzesView.vue"
 </script>
 
 <style scoped>
 .container {
   max-width: 1000px;
   margin: 0 auto;
-}
-
-header {
-  margin-bottom: 10px;
-  margin-top: 30px;
-  display: flex;
-  align-items: center;
-}
-
-header h1 {
-  font-weight: bold;
-  margin-right: 30px;
-}
-
-header input {
-  border: none;
-  background-color: rgba(128, 128, 128, 0.1);
-  padding: 10px;
-  border-radius: 5px;
-}
-
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
 }
 </style>
