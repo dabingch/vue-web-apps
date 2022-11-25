b816429b081d242ac27450970958a9d3838df06b
Hao Chen <hche8093@uni.sydney.edu.au>
2022-11-26 10:57:31 +1100

    Initial vue-state-app

---
+++ vue-state-app/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div>
+      <input type="number" placeholder="red">
+      <input type="number" placeholder="blue">
+      <input type="number" placeholder="green">
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: rgb(122, 50, 255);
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  display: flex;
+  flex-direction: column;
+}
+</style>
