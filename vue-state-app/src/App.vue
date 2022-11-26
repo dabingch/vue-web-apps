8f7aad2eb0ec55af25a8dba602c3552ec58f4d05
Hao Chen <hche8093@uni.sydney.edu.au>
2022-11-26 11:09:04 +1100

    Add background color

--- vue-state-app/src/App.vue
+++ vue-state-app/src/App.vue
@@ -1,19 +1,37 @@
 <script setup>
+import { ref, reactive } from 'vue';
+
+// const red = ref(0)
+// const blue = ref(0)
+// const green = ref(0)
+
+// const color = ref({
+//   red: 0,
+//   blue: 0,
+//   green: 0
+// })
+
+// Only takes objects
+const color = reactive({
+  red: 0,
+  blue: 0,
+  green: 0
+})
 </script>
 
 <template>
-  <main>
+  <main :style="{backgroundColor: `rgb(${color.red},${color.green},${color.blue})`}">
     <div>
-      <input type="number" placeholder="red">
-      <input type="number" placeholder="blue">
-      <input type="number" placeholder="green">
+      <input type="number" v-model="color.red" placeholder="red">
+      <input type="number" v-model="color.blue" placeholder="blue">
+      <input type="number" v-model="color.green" placeholder="green">
     </div>
   </main>
 </template>
 
 <style scoped>
 main {
-  background-color: rgb(122, 50, 255);
+  /* background-color: rgb(122, 50, 255); */
   height: 100vh;
   display: flex;
   align-items: center;
