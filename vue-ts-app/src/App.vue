23efbdba2f0194e314b3c156f6daec1590b4f559
Hao Chen <hche8093@uni.sydney.edu.au>
2022-11-28 10:46:10 +1100

    Add invitees functionality in vue-ts-app

--- vue-ts-app/src/App.vue
+++ vue-ts-app/src/App.vue
@@ -1,14 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+enum GENDER {
+  MALE,
+  FEMALE,
+}
+
+const name = ref("");
+
+interface Invitee {
+  id: number;
+  name: string;
+  gender: GENDER;
+}
+
+const addInvitee = (): void => {
+  if (name.value) {
+    invitees.value.push({
+      id: Math.floor(Math.random() * 100000),
+      name: name.value,
+      gender: gender.value,
+    });
+  }
+};
+
+const gender = ref(GENDER.MALE);
+
+const invitees = ref<Invitee[]>([]);
+</script>
 
 <template>
   <main>
     <div>
       <h1>People Invited to My Party</h1>
       <div class="input-container">
-        <input type="text" placeholder="Name..." />
-        <select>
-          <option value="">Male</option>
-          <option value="">Female</option>
+        <input
+          type="text"
+          placeholder="Name..."
+          v-model="name"
+          @keypress.enter="addInvitee"
+        />
+        <select v-model="gender" @keypress.enter="addInvitee">
+          <option :value="GENDER.MALE">Male</option>
+          <option :value="GENDER.FEMALE">Female</option>
         </select>
       </div>
     </div>
