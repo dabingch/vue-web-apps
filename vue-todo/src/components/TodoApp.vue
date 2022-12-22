4529e0e9718e80ac67935a8c88dae3d8e012ec82
Hao Chen <hche8093@uni.sydney.edu.au>
2022-12-23 10:25:02 +1100

    Initial vue-todo app

---
+++ vue-todo/src/components/TodoApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="todo-app">
+    <h1>To Do List</h1>
+    <TodoForm />
+    <TodoList />
+  </div>
+</template>
+
+<script setup>
+import TodoForm from './TodoForm.vue'
+import TodoList from './TodoList.vue'
+</script>
+
+<style scoped>
+
+</style>
