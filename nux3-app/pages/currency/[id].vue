9095201d55b29f87557c484b139c01c529d284e0
Hao Chen <hche8093@uni.sydney.edu.au>
2022-12-24 11:04:58 +1100

    Add nuxt3-essential-app

---
+++ nux3-app/pages/currency/[id].vue
@@ -0,0 +1,25 @@
+<script setup>
+const route = useRoute()
+const { data } = await useFetch('/api/ticker?id=' + route.params.id)
+const coin = data.value[0]
+</script>
+
+<template>
+  <div>
+    <h2>{{ coin.name }} Detail page</h2>
+    <table border="1 px solid">
+      <thead>
+        <th>Symbol</th>
+        <th>Rank</th>
+        <th>Price - US $</th>
+        <th>Market Cap - US $</th>
+      </thead>
+      <tr>
+        <td>{{ coin.symbol }}</td>
+        <td>{{ coin.rank }}</td>
+        <td>{{ coin.price_usd }}</td>
+        <td>{{ coin.market_cap_usd }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
---
+++ nux3-app/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <template>
+    <main>
+      <h1>Index Page</h1>
+  
+      <table border="1 px solid">
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Symbol</th>
+          <th>Price</th>
+          <th>Details</th>
+        </tr>
+        </thead>
+        <tr v-for="currency in currencies" :key="currency.id">
+          <td>{{ currency.name }}</td>
+          <td>{{ currency.symbol }}</td>
+          <td>{{ currency.price_usd }}</td>
+          <td>
+            <NuxtLink :to="'/currency/' + currency.id">{{ currency.id }}</NuxtLink>
+          </td>
+        </tr>
+      </table>
+    </main>
+  </template>
+</template>
+
+<script setup>
+const { data } = await useFetch('/api/tickers?limit=10')
+const currencies = data._value.data
+console.log(currencies)
+</script>
