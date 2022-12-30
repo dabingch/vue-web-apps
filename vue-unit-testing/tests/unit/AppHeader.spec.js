484f225e1d7d08dad2313e4f64cb372902f003eb
Hao Chen <hche8093@uni.sydney.edu.au>
2022-12-30 11:24:21 +1100

    Add vue-unit-testing

---
+++ vue-unit-testing/tests/unit/AppHeader.spec.js
@@ -0,0 +1,17 @@
+import AppHeader from "@/components/AppHeader";
+import { mount } from "@vue/test-utils";
+
+describe("AppHeader", () => {});
+
+test("if user is not logged in, do not show logout button", () => {
+	const wrapper = mount(AppHeader);
+	// expect(true).toBe(true);
+	expect(wrapper.find("button").isVisible()).toBe(false);
+});
+
+test("if a user is logged in, show logout button", async () => {
+	const wrapper = mount(AppHeader);
+	// expect(true).toBe(true);
+	await wrapper.setData({ loggedIn: true });
+	expect(wrapper.find("button").isVisible()).toBe(true);
+});
