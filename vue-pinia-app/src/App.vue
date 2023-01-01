59cde8e6b155e38d76e8552c1cea18350d3ba375
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-01 15:01:31 +1100

    Add task component in vue-pinia-app

--- vue-pinia-app/src/App.vue
+++ vue-pinia-app/src/App.vue
@@ -1,11 +1,22 @@
-<script setup></script>
+<script setup>
+import { useTaskStore } from './stores/taskStore'
+import TaskDetails from './components/TaskDetails.vue'
+
+const taskStore = useTaskStore()
+</script>
 
 <template>
 	<main>
 		<header>
 			<img src="./assets/pinia-logo.svg" alt="pinia logo" />
-			<h2>Pinia Tasks</h2>
+			<h2>{{ taskStore.name }}</h2>
 		</header>
+
+		<div class="task-list">
+			<div v-for="task in taskStore.tasks" :key="task.id">
+				<TaskDetails :task="task" />
+			</div>
+		</div>
 	</main>
 </template>
 
--- vue-pinia-app/src/stores/taskStore.js
+++ vue-pinia-app/src/stores/taskStore.js
@@ -1,10 +1,11 @@
 import { defineStore } from 'pinia'
 
-export const useTaskStore = defindStore('task', {
+export const useTaskStore = defineStore('task', {
 	state: () => ({
 		tasks: [
 			{ id: 1, title: 'Task 1', isFav: false },
 			{ id: 2, title: 'Task 2', isFav: false },
 		],
+		name: 'Pinia Tasks',
 	}),
 })
