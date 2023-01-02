07cb0494b223af68af73256307a8cbaff7f43d70
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-02 11:53:20 +1100

    Add layouts in vue-nuxt-app

---
+++ vue-nuxi-dojo/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<header>
+			<nav>
+				<ul>
+					<li><NuxtLink to="/">Home</NuxtLink></li>
+					<li><NuxtLink to="/about">About</NuxtLink></li>
+					<li><NuxtLink to="/products">Products</NuxtLink></li>
+				</ul>
+			</nav>
+		</header>
+
+		<div><slot></slot></div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.router-link-exact-active {
+	color: #12b488;
+}
+</style>
