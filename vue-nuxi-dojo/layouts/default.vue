f6d87c452732560be30d137f97aba31179805e7c
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-02 12:05:25 +1100

    Add tailwind css in vue-nuxt-app

--- vue-nuxi-dojo/layouts/default.vue
+++ vue-nuxi-dojo/layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
 	<div>
-		<header>
-			<nav>
-				<ul>
+		<header class="shadow-sm bg-white">
+			<nav class="container mx-auto p-4 flex justify-between">
+				<NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
+				<ul class="flex gap-4">
 					<li><NuxtLink to="/">Home</NuxtLink></li>
 					<li><NuxtLink to="/about">About</NuxtLink></li>
-					<li><NuxtLink to="/products">Products</NuxtLink></li>
+					<li>
+						<NuxtLink to="/products" class="btn">Products</NuxtLink>
+					</li>
 				</ul>
 			</nav>
 		</header>
 
-		<div><slot></slot></div>
+		<div class="container mx-auto p-4"><slot /></div>
 	</div>
 </template>
 
