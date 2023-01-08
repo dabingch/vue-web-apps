fd8a6e105767fc9473e81d8fdddbaf8e84e099c7
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-08 12:31:48 +1100

    Add card component and img in vue-pinia-nuxt

--- vue-pinia-nuxt/pages/index.vue
+++ vue-pinia-nuxt/pages/index.vue
@@ -1,9 +1,18 @@
 <template>
 	<div>
 		<h2 class="text-white text-2xl text-center my-8">All Products</h2>
+		<div class="grid grid-cols-2 gap-7">
+			<div v-for="product in products" :key="product.id">
+				<ProductCard :product="product" />
+			</div>
+		</div>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import ProductCard from '@/components/ProductCard.vue'
+
+const { data: products } = await useFetch('http://localhost:4000/products')
+</script>
 
 <style scoped></style>
