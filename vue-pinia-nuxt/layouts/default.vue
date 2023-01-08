35a10507446145c04dd61c30e7158a003a8cf9bd
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-08 11:44:28 +1100

    Add tailwind in vue-pinia-nuxt

--- vue-pinia-nuxt/layouts/default.vue
+++ vue-pinia-nuxt/layouts/default.vue
@@ -1,13 +1,19 @@
 <template>
-	<div>
-		<header>
-			<NuxtLink to="/"><h1>Adventure Store</h1></NuxtLink>
+	<div class="m-8">
+		<header class="wrapper flex justify-between">
+			<NuxtLink to="/"
+				><h1 class="text-secondary text-5xl">
+					Adventure Store
+				</h1></NuxtLink
+			>
 			<NuxtLink to="/cart">
-				<span class="material-symbols-outlined"> shopping_bag </span>
+				<span class="material-symbols-outlined text-secondary text-5xl">
+					shopping_bag
+				</span>
 			</NuxtLink>
 		</header>
 
-		<div>
+		<div class="wrapper">
 			<slot />
 		</div>
 	</div>
