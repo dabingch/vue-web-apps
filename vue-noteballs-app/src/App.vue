49a48c807c7fdb96eec9e1ddfd24ecae4cbfea0a
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-18 17:02:00 +1100

    Config vue-router

--- vue-noteballs-app/src/App.vue
+++ vue-noteballs-app/src/App.vue
@@ -3,6 +3,8 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+	<RouterLink to="/">Notes</RouterLink> |
+	<RouterLink to="/stats">Stats</RouterLink>
 	<RouterView />
 </template>
 
---
+++ vue-noteballs-app/src/views/VueStatsView.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="stats">
+		<h1>Status</h1>
+	</div>
+</template>
+
+<style></style>
